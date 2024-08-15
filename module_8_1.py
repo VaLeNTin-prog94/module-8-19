08aaea64db37fc226c919cdbc3b8fa421c99784c
VaLeNTin-prog94 <valentinforevo@gmail.com>
2024-08-15 12:57:16 +0300

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -1,10 +1,30 @@
+def k_count(n):
+    return len(str(n).replace('.', ' ').split()[-1])
+
+
+def type_a_b(a, b):
+    if type(a) == float and type(b) != float:
+        return 1
+    elif type(b) == float and type(a) != float:
+        return 2
+    elif type(a) == float and type(a) == float:
+        return 3
+
+
 def add_everything_up(a, b):
     try:
-        return round(a + b, 3)
+        match type_a_b(a, b):
+            case 1:
+                return round(a + b, k_count(a))
+            case 2:
+                return round(a + b, k_count(b))
+            case 3:
+                return round(a + b, k_count(a) if k_count(a) > k_count(b) else k_count(b))
+
     except TypeError:
         return str(a) + str(b)
 
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+print(add_everything_up(123.7676, 7.4343434))
